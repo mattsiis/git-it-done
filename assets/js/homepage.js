d7c93fb9143d1766eae8ca41c4702acfd519f494
mattsiis <35969076+mattsiis@users.noreply.github.com>
2021-08-03 01:59:13 -0400

    error check

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -12,10 +12,13 @@ var getUserRepos = function (user) {
             alert ("Error: " + response.statusText);
         }
     })
+    .catch(function(error) {
+        alert("Unable to connect to Github")
+    })
 }
-
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
+
 
 var formSubmitHandler = function(event) {
     event.preventDefault();
@@ -51,8 +54,11 @@ var displayRepos = function(repos, searchTerm){
         var repoName = repos[i].owner.login + "/" + repos[i].name;
 
         // create a container for each repo
-        var repoEl = document.createElement("div");
+        var repoEl = document.createElement("a");
         repoEl.classList = "list-item flex-row justify-space-between align-center";
+        repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+        // create a span element to hold repository name
+
 
         // create a span element to hold repository name
         var titleEl = document.createElement("span");
